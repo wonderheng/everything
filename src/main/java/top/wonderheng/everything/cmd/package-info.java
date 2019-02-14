706d948362dbc2778d0ac33701a9c359f870eee1
WonderHeng <995107866@qq.com>
2019-02-14 11:28:54 +0800

    添加ignore文件

---
+++ src/main/java/top/wonderheng/everything/cmd/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @BelongsProject: everything
+ * @BelongsPackage: top.wonderheng.everything.cmd
+ * @Author: WonderHeng
+ * @CreateTime: 2019-02-14 10:34
+ */
+package top.wonderheng.everything.cmd;
+
+/*
+ * cmd包中主要放置everything-plus应用程序的入口程序
+ */
